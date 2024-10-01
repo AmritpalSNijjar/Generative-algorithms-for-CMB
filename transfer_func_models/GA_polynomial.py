59c831f573908cbfcf3ea313c58261564b61c776
Amritpal Nijjar <amritnijjar19@gmail.com>
2024-10-01 14:53:22 -0400

    first version of polynomial GA, requires tuning

--- transfer_func_models/GA_polynomial.py
+++ transfer_func_models/GA_polynomial.py
@@ -48,10 +48,10 @@ class candPolynomial():
             self.second_order_orders[i, 1] = b
             
             if b == self.n_vars - 1:
-                    a += 1
-                    b  = a + 1
-                else:
-                    b += 1
+                a += 1
+                b  = a + 1
+            else:
+                b += 1
                     
         self.offset_range              = np.array([-10, 10]) 
         
@@ -60,6 +60,17 @@ class candPolynomial():
         
         self.second_order_coeff_ranges = np.array([[-2, 2] for i in range(self.n_second_order_terms)])
         self.second_order_exp_ranges   = np.array([[0, 3] for i in range(2*self.n_second_order_terms)])
+    
+    def represent(self):
+        representation = f"p = {self.offset} +"
+        for i in range(self.n_first_order_terms):
+            if self.first_order_keeps[i] == 1:
+                representation += f" {self.first_order_coeffs[i]}*{self.var_names[i]}^{self.first_order_exps[i]} +"
+        for i in range(self.n_second_order_terms):
+            if self.second_order_keeps[i] == 1:
+                representation += f" {self.second_order_coeffs[i]}*({self.var_names[self.second_order_orders[i, 0]]}^{self.second_order_exps[2*i]})*({self.var_names[self.second_order_orders[i, 1]]}^{self.second_order_exps[2*i+1]}) +"
+        representation = representation[:-1] + "."
+        print(representation)
     
     def set_params(self, params_dict):
         
@@ -143,15 +154,19 @@ class candPolynomial():
         
         n2 = self.n_second_order_terms
         
-        if self.order == "second:"
-            second_coeff_inds = [j + 2*n1 for j in range(n2) if self.second_order_keeps[i] == 1]   # [2*n1,..., 2*n1 + n2 - 1]
-            second_exp_inds = [j + 2*n1 + n2 for j in range(n2) if self.second_order_keeps[i] == 1]# [2*n1 + n2,..., 2*n1 + 2*n2 - 1]
+        if self.order == "second":
+            second_coeff_inds = [j + 2*n1 for j in range(n2) if self.second_order_keeps[j] == 1]   # [2*n1,..., 2*n1 + n2 - 1]
+            second_exp_inds = [j + 2*n1 + n2 for j in range(n2) if self.second_order_keeps[j] == 1]# [2*n1 + n2,..., 2*n1 + 2*n2 - 1]
             inds = inds + second_coeff_inds + second_exp_inds
+            
         
         if self.learn_offset:
-            inds.append(inds[-1] + 1)
+            if len(inds) == 0:
+                inds = [0]
+            else:
+                inds.append(inds[-1] + 1)
         
-        mutate_inds = np.random.choice(inds, ngenes_to_mutate, replace = False)
+        mutate_inds = np.random.choice(inds, min(ngenes_to_mutate, len(inds)), replace = False)
         
         for i in mutate_inds:
             
@@ -168,7 +183,7 @@ class candPolynomial():
                 j = i - n1
                 self.first_order_exps[j] = np.random.uniform(self.first_order_exp_ranges[j, 0], self.first_order_exp_ranges[j, 1])
             else:
-                self.first_order_coeffs[j] = np.random.uniform(self.first_order_coeff_ranges[j, 0], self.first_order_coeff_ranges[j, 1])
+                self.first_order_coeffs[i] = np.random.uniform(self.first_order_coeff_ranges[i, 0], self.first_order_coeff_ranges[i, 1])
     
     def compute(self, vars_dict):
         
@@ -183,7 +198,7 @@ class candPolynomial():
             
             for i in range(self.n_second_order_terms):
                 
-                val += self.second_order_keeps[i] * self.second_order_coeffs[i] * (vars_dict[self.var_names[self.second_order_orders[i, 0]]] ** self.second_order_coeffs[2*i]) * (vars_dict[self.var_names[self.second_order_orders[i, 1]]] ** self.second_order_coeffs[2*i + 1])
+                val += self.second_order_keeps[i] * self.second_order_coeffs[i] * (vars_dict[self.var_names[self.second_order_orders[i, 0]]] ** self.second_order_exps[2*i]) * (vars_dict[self.var_names[self.second_order_orders[i, 1]]] ** self.second_order_exps[2*i + 1])
         
         return val
 
@@ -195,6 +210,9 @@ class dataPoint():
         
         self.params_dict = params_dict # could have any params, including ones not being modelled by GA. but make sure the 
                                        # string names of the params used in GA polynomials are the same strings here
+            
+            
+        # (x_var, y_var) ARE THE POINTS OF WHAT'S TO BE MODELLED
         self.x_var = x_var # x-variable to make the plot we want to model
         self.y_var = y_var # y-variable values of the true (simulated) plot
 
@@ -248,26 +266,24 @@ class geneticAlgorithmPolynomial():
         for i in range(self.population):
             self.kids[i] = candPolynomial(var_names = self.var_names, order = self.order, learn_offset = self.learn_offset)
             self.kids[i].randomize_params()
-            if self.set_ranges:
-                self.kids[i].set_ranges(self.ranges_dict)
+            
+            #if self.set_ranges:
+            #    self.kids[i].set_ranges(self.ranges_dict)
         
     def run_algorithm(self):
         
         # generate random initial population
         self.init_pop()
         
-        
         print("Starting genetic algorithm...")
         for i in range(self.generations):
             
-            self.sort_winners(i)
+            self.tournament(i)
             if i%10 == 0:
                 print(f"Generation: {i + 1}")
-                print(f"Most fit: {self.best_fit_per_gen[i]}")
+                print(f"Best Fitness: {self.best_fit_per_gen[i]}")
             
             # a & b denote the indices of the parent functions to breed kid functions from
-            # 0 is the most fit function, 1 is the second most fit, etc.
-            # the current algorithm works as follows: if there were possible parent functions 0, 1, 2, 3, 4
             
             # kids1, kids2 = crossbreed(parent0, parent1)
             # kids3, kids4 = crossbreed(parent0, parent2)
@@ -275,7 +291,6 @@ class geneticAlgorithmPolynomial():
             # kids7, kids8 = crossbreed(parent0, parent4)
             # kids9, kids10 = crossbreed(parent1, parent2)
             # ...
-            
             
             a = 0
             b = 1
@@ -297,40 +312,92 @@ class geneticAlgorithmPolynomial():
             
             for i in range(0, self.population):
                 if np.random.uniform() <= self.mutation_rate:
-                    self.kids[i].mutate(ngenes_to_mutate = self.ngenes_to_mutate, range_mutate = True)
+                    if np.random.uniform() <= 0.5:
+                        self.kids[i].mutate_params(ngenes_to_mutate = 3)
+                    else:
+                        self.kids[i].mutate_form(nterms_to_flip = 1)
         
         
-        self.sort_winners(i)
+        self.tournament(i)
         self.best_fit_func = self.kids[0]
     
     def compute_fitness(self, candPoly: candPolynomial):
         
-        fitness = 0
-        
         #TO DO: For quick and rough fitness computation, for example if num_points is large, we can
         #       instead sample a number of points from the data instead of using it all...
         
-        num_points = self.camb_data.shape[1]
+        N = 0
         
-        # camb_data file will look like:
-        # [[ommh2], [ombh2], [k], [T(k)]]
+        for dat in self.train_data:
+            
+            polynomial_eval = candPoly.compute(dat.params_dict)
+            
+            eval_at_points  = dat.x_var
+            N              += len(eval_at_points)
+            truth           = dat.y_var
+
+            # modeled_f IS HARD-CODED... WHERE DOES THE POLYNOMIAL COME INTO THE FUNCTIONAL FORM OF THE MODEL
+            # in this case..... our modeled func is:
+            # f(k; a, b, ...) = k ^ p(a, b, ...)  where p(a, b, ...) would be some polynomial of parameters a, b, ...
+
+            modeled_f = eval_at_points**(polynomial_eval) 
+            # ^(TODO: MAKE MODELED_F A FUNCTION THAT IS FED INTO THE GA THROUGH THE CONFIG FILE...... LIKE SELF.MODEL_SKELETON) 
+
+            val = np.abs((modeled_f - truth)/truth)
+            
+            fitness = np.sum(val)
         
-        for i in range(num_points):
-            val = np.abs(self.camb_data[3][i] - candFunc.compute(self.camb_data[0][i], self.camb_data[1][i], self.camb_data[2][i]))
-            val /= self.camb_data[3][i]
-            fitness += val
+        fitness *= 100/N
         
-        # Eq. (16)
-        fitness *= 100/num_points
         return fitness
     
-    def save_top_func(self):
-        # TO DO: Auto save the best function... text file with just params?
-        # best_function = self.kids[-1, 0]
-        # pickle save:
-        # As = best_function.getAs
-        # Cs = best_function.getCs
-        return 0
+    def tournament(self, cur_gen):
+        
+        #k: TOURNAMENT SUBGROUP SIZE
+        k = int(0.3*self.population) # => k is 30% of the total popluation... TODO: MAKE THIS A HYPERPARAMETER
+        
+        #p: SUBGROUP SAMPLING PROPBABILITY
+        p = 0.75 # TODO: MAKE THIS A HYPERPARAMETER
+        
+        #create temporary array to store tournament winners
+        tournament_winners = np.empty(self.num_winners, dtype=candPolynomial)
+        
+        inds = [i for i in range(self.population)]
+        
+        t_best_fitness = 0
+        
+        for i in range(self.num_winners):
+            # Tournament round
+            
+            subgroup_inds = np.random.choice(inds, k, replace = False)
+            
+            subgroup_fitnesses = [self.compute_fitness(self.kids[ind]) for ind in subgroup_inds]
+            
+            subgroup_argsort = np.argsort(subgroup_fitnesses)
+            subgroup_fitness_rankings = np.argsort(subgroup_argsort)
+            probabilities = p*(1-p)**subgroup_fitness_rankings
+            
+            # To make sure probabilities array sums to 1
+            diff_from_one = 1 - np.sum(probabilities)
+            probabilities[0] += diff_from_one
+            
+            winner_ind = np.random.choice(subgroup_inds, size = None, p = probabilities)
+            
+            tournament_winners[i] = self.kids[winner_ind]
+            
+            winner_ind_in_subgroup = np.where(subgroup_inds == winner_ind)[0][0]
+            winner_fitness = subgroup_fitnesses[winner_ind_in_subgroup]
+            
+            if i == 0: 
+                t_best_fitness = winner_fitness
+            
+            t_best_fitness = min(t_best_fitness, winner_fitness)
+        
+        for i in range(self.num_winners):
+            self.kids[i] = tournament_winners[i]
+        
+        self.best_fit_per_gen[cur_gen] = t_best_fitness
+    
     
     def sort_winners(self, cur_gen):
         
@@ -342,27 +409,53 @@ class geneticAlgorithmPolynomial():
         sorted_kids = [self.kids[inds[i]] for i in range(self.population)]
         self.kids = np.array(sorted_kids)
     
-    def crossbreed(self, parent1: candFunction, parent2: candFunction) -> tuple[candFunction, candFunction]:
-    
-        par1_genes = parent1.params
-        par2_genes = parent2.params
-
-
-        # This part can be reworked to have random genes in the genome swap instead of a 
-        # 50/50 split down the middle
-        genes_swap_length = parent1.tot_genes//2
-
-        kid1_genes = np.concatenate((par1_genes[:genes_swap_length], par2_genes[genes_swap_length:]))
-        kid2_genes = np.concatenate((par2_genes[:genes_swap_length], par1_genes[genes_swap_length:]))
-        # --------------------------------------------------
-
-        kid1 = candFunction()
-        kid2 = candFunction()
-        kid1.set_params(kid1_genes)
-        kid2.set_params(kid2_genes)
+    def crossbreed(self, parent1: candPolynomial, parent2: candPolynomial):
         
-        if self.set_ranges:
-            kid1.set_ranges(self.ranges)
-            kid2.set_ranges(self.ranges)
+        par1_first_order_coeffs  = parent1.first_order_coeffs
+        par1_first_order_exps    = parent1.first_order_exps
+        par1_second_order_coeffs = parent1.second_order_coeffs
+        par1_second_order_exps   = parent1.second_order_exps
+        
+        par2_first_order_coeffs  = parent2.first_order_coeffs
+        par2_first_order_exps    = parent2.first_order_exps
+        par2_second_order_coeffs = parent2.second_order_coeffs
+        par2_second_order_exps   = parent2.second_order_exps
+        
+        kid1_params_dict = {}
+        kid2_params_dict = {}
+        
+        kid1_params_dict["first_order_keeps"]   = parent1.first_order_keeps
+        kid1_params_dict["second_order_keeps"]  = parent1.second_order_keeps
+        
+        kid2_params_dict["first_order_keeps"]   = parent2.first_order_keeps
+        kid2_params_dict["second_order_keeps"]  = parent2.second_order_keeps
+        
+        kid1_params_dict["offset"]              = parent2.offset
+        kid2_params_dict["offset"]              = parent1.offset
+        
+        L = len(par1_first_order_coeffs)//2
+        kid1_params_dict["first_order_coeffs"]  = np.concatenate((par1_first_order_coeffs[:L], par2_first_order_coeffs[L:]))
+        kid2_params_dict["first_order_coeffs"]  = np.concatenate((par2_first_order_coeffs[:L], par1_first_order_coeffs[L:]))
+        
+        L = len(par1_first_order_exps)//2
+        kid1_params_dict["first_order_exps"]    = np.concatenate((par1_first_order_exps[:L], par2_first_order_exps[L:]))
+        kid2_params_dict["first_order_exps"]    = np.concatenate((par2_first_order_exps[:L], par1_first_order_exps[L:]))
+        
+        L = len(par1_second_order_coeffs)//2
+        kid1_params_dict["second_order_coeffs"] = np.concatenate((par1_second_order_coeffs[:L], par2_second_order_coeffs[L:]))
+        kid2_params_dict["second_order_coeffs"] = np.concatenate((par2_second_order_coeffs[:L], par1_second_order_coeffs[L:]))
+        
+        L = len(par1_second_order_exps)//2
+        kid1_params_dict["second_order_exps"]   = np.concatenate((par1_second_order_exps[:L], par2_second_order_exps[L:]))
+        kid2_params_dict["second_order_exps"]   = np.concatenate((par2_second_order_exps[:L], par1_second_order_exps[L:]))
+
+        kid1 = candPolynomial(self.var_names, order = self.order, learn_offset = self.learn_offset)
+        kid2 = candPolynomial(self.var_names, order = self.order, learn_offset = self.learn_offset)
+        kid1.set_params(kid1_params_dict)
+        kid2.set_params(kid2_params_dict)
+        
+        #if self.set_ranges:
+        #    kid1.set_ranges(self.ranges)
+        #    kid2.set_ranges(self.ranges)
             
         return kid1, kid2
